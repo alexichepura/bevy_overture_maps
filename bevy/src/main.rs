e7da175166dcd35ed3036241f8eb69bf9f9ce598
Alexi Chepura <a.chepura@gmail.com>
2023-08-04 13:08:26 +0300

    env, lat lng, transportation

--- bevy/src/main.rs
+++ bevy/src/main.rs
@@ -6,24 +6,43 @@ mod bevy;
 mod building;
 mod camera;
 mod duckdb_query;
-mod query_transportation;
-mod transportation;
 mod ground;
 mod light;
 mod parquet_import;
+mod query_transportation;
+mod transportation;
+
+// https://docs.overturemaps.org/
+// https://www.ogc.org/standard/sfa/
+// https://duckdb.org/docs/data/multiple_files/overview
 
 fn main() {
-    // https://docs.overturemaps.org/
-    // https://www.ogc.org/standard/sfa/
-    let bevy_buildings = duckdb_query_buildings(BuildingsQueryParams { 
-        limit: 1000, 
-        from_string: "read_parquet('./buildings-japan.parquet')".to_string(),
-        where_string: "ST_Within(ST_GeomFromWkb(geometry), ST_Envelope(ST_GeomFromText('POLYGON((139.68170 35.67951, 139.68170 35.69951, 139.70170 35.69951, 139.70170 35.67951, 139.68170 35.67951))')))".to_string() 
+    let lat = std::env::var("BEVY_OVERTURE_LAT").expect("BEVY_OVERTURE_LAT env");
+    dbg!(&lat);
+    let lat = lat.parse::<f64>().expect("lat to be f64");
+
+    let lng = std::env::var("BEVY_OVERTURE_LNG").expect("BEVY_OVERTURE_LNG env");
+    let lng = lng.parse::<f64>().expect("lng to be f64");
+
+    let shift = 0.01;
+    let lat_max = lat + shift;
+    let lat_min = lat - shift;
+    let lng_max = lng + shift;
+    let lng_min = lng - shift;
+
+    let polygon_str = format!("{lng_min} {lat_min}, {lng_min} {lat_max}, {lng_max} {lat_max}, {lng_max} {lat_min}, {lng_min} {lat_min}", );
+    let query = format!("ST_Within(ST_GeomFromWkb(geometry), ST_Envelope(ST_GeomFromText('POLYGON(({polygon_str}))')))");
+
+    let bevy_transportation = duckdb_query_transportation(TransportationQueryParams {
+        limit: 2,
+        from_string: "read_parquet('../overture/type=segment/*')".to_string(),
+        where_string: query.clone(),
     });
-    let bevy_transportation = duckdb_query_transportation(TransportationQueryParams { 
-        limit: 1, 
-        from_string: "read_parquet('./transportation-connector-japan.parquet')".to_string(),
-        where_string: "ST_Within(ST_GeomFromWkb(geometry), ST_Envelope(ST_GeomFromText('POLYGON((139.68170 35.67951, 139.68170 35.69951, 139.70170 35.69951, 139.70170 35.67951, 139.68170 35.67951))')))".to_string() 
+
+    let bevy_buildings = duckdb_query_buildings(BuildingsQueryParams {
+        limit: 2,
+        from_string: "read_parquet('../overture/theme=buildings/type=building/*')".to_string(),
+        where_string: query,
     });
     init_bevy(bevy_buildings, bevy_transportation);
 }
--- bevy/src/query_transportation.rs
+++ bevy/src/query_transportation.rs
@@ -20,42 +20,89 @@ pub fn duckdb_query_transportation(params: TransportationQueryParams) -> Vec<Bev
     let limit = params.limit;
     let where_string = params.where_string;
     let from = params.from_string;
+    println!("statement for building - start");
     let mut stmt = conn
-        .prepare(
-            &format!(
-                "SELECT
+        .prepare(&format!(
+            "SELECT
                 id,
-                height,
-                JSON(names) as names,
+                subtype,
                 ST_GeomFromWkb(geometry) AS geometry
-            FROM {from}
-            WHERE {where_string}
-            LIMIT {limit}"
-            ),
-            // WHERE
-            //     bbox.minX > 139.69170 AND bbox.maxX < 139.70170 AND bbox.minY > 35.68951 AND bbox.maxY < 35.69951",
-        )
+                FROM {from}
+                WHERE {where_string}
+                LIMIT {limit}"
+        ))
+        // connectors,
+        // road,
+        // "SELECT
+        //     id,
+        //     updatetime,
+        //     version,
+        //     level,
+        //     subtype,
+        //     connectors,
+        //     road,
+        //     sources,
+        // FROM {from}
+        // WHERE {where_string}
+        // LIMIT {limit}"
         .unwrap();
-    println!("success SELECT");
+    println!("statement for building - end");
     #[derive(Debug)]
-    struct Bb {
+    struct Transportation {
         id: String,
-        // height: Option<f64>,
-        // names: String,
-        // bbox: String,
-        // geom: Vec<u8>,
+        subtype: Option<String>,
+        geom: Vec<u8>,
+        // connectors: Option<String>,
+        // road: Option<String>,
     }
     let query_iter = stmt
         .query_map([], |row| {
-            Ok(Bb {
+            Ok(Transportation {
                 id: row.get(0)?,
-                // height: row.get(1)?,
-                // names: row.get(2)?,
-                // bbox: row.get(3)?,
-                // geom: row.get(3)?,
+                subtype: row.get(1)?,
+                geom: row.get(2)?,
+                // connectors: row.get(2)?,
+                // road: row.get(3)?,
             })
         })
         .unwrap();
+
+    println!("statement for building - query loaded");
+
+    for item in query_iter {
+        let item = item.unwrap();
+        // dbg!(&item);
+        let raw = item.geom;
+        // MAGIC TO GET ARRAY THAT WORKS, COMPARED TO BINARY FROM PARQUET DIRECTLY
+        let raw = &raw[21..]; // remove 0, 2, 96, 0, 0, 0, 0, 0, 2, 0, 0, 0, 1, 0, 0, 0, 5, 0, 0, 0, 0,
+        let prefix: [u8; 10] = [1, 3, 0, 0, 0, 1, 0, 0, 0, 5];
+        let raw = [prefix.as_slice(), raw].concat();
+        let mut rdr = std::io::Cursor::new(raw);
+        let g = Geometry::from_wkb(&mut rdr, WkbDialect::Wkb);
+        match g {
+            Ok(g) => match g {
+                Geometry::Polygon(polygon) => {
+                    dbg!(polygon);
+                    // if !is_base_set {
+                    //     let (k, pos) = polygon_base(&polygon);
+                    //     base_pos = pos;
+                    //     base_k = k;
+                    //     is_base_set = true;
+                    // }
+                    // let bevy_building =
+                    //     polygon_building(polygon, base_k, base_pos, query_item.height);
+                    // bevy_buildings.push(bevy_building);
+                }
+                not_polygon => {
+                    dbg!(&not_polygon);
+                }
+            },
+            Err(e) => {
+                dbg!(e);
+            }
+        }
+    }
+
     let mut bevy_transportations: Vec<BevyTransportation> = vec![];
     bevy_transportations
 }
