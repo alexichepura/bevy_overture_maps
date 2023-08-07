b0c058fcae4e7783bc4af50e9443b667a4d21d2d
Alexi Chepura <a.chepura@gmail.com>
2023-08-07 09:20:50 +0300

    COPY to parquet

--- cli/src/db.rs
+++ cli/src/db.rs
@@ -8,8 +8,7 @@ pub fn cache_location(lon: f64, lat: f64) {
     let lon_min = lon - shift;
 
     let polygon_str = format!("{lon_min} {lat_min}, {lon_min} {lat_max}, {lon_max} {lat_max}, {lon_max} {lat_min}, {lon_min} {lat_min}", );
-    let query = format!("ST_Within(ST_GeomFromWkb(geometry), ST_Envelope(ST_GeomFromText('POLYGON(({polygon_str}))')))");
-    let from_string = "read_parquet('../overture/type=segment/*')".to_string();
+    let where_geometry = format!("ST_Within(ST_GeomFromWkb(geometry), ST_Envelope(ST_GeomFromText('POLYGON(({polygon_str}))')))");
 
     let path = "./data.duckdb";
     let conn = Connection::open(&path).unwrap();
@@ -17,15 +16,21 @@ pub fn cache_location(lon: f64, lat: f64) {
     conn.execute_batch("INSTALL spatial; LOAD spatial;")
         .unwrap();
 
+    let from_segment = "read_parquet('../overture/type=segment/*')".to_string();
     let mut stmt = conn
         .prepare(&format!(
-            "COPY (SELECT id,
-                ST_GeomFromWkb(geometry) AS geometry,
-                road
-                FROM {from_string} WHERE {query})
+            "COPY (SELECT * FROM {from_segment} WHERE {where_geometry})
             TO 'parquet/result-transportation.parquet' (FORMAT 'parquet')"
         ))
         .unwrap();
+    let _ = stmt.query([]).unwrap();
 
-    stmt.query([]).unwrap();
+    let from_building = "read_parquet('../overture/theme=buildings/type=building/*')".to_string();
+    let mut stmt = conn
+        .prepare(&format!(
+            "COPY (SELECT * FROM {from_building} WHERE {where_geometry})
+            TO 'parquet/result-building.parquet' (FORMAT 'parquet')"
+        ))
+        .unwrap();
+    let _ = stmt.query([]).unwrap();
 }
