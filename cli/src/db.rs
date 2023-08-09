4ee236f8b93f493b4a50526e3a6b4dce8b086201
Alexi Chepura <a.chepura@gmail.com>
2023-08-09 19:12:31 +0300

    faster import with bbox

--- cli/src/db.rs
+++ cli/src/db.rs
@@ -2,13 +2,14 @@ use duckdb::Connection;
 
 pub fn cache_location(lon: f64, lat: f64, name: &str) {
     let shift = 0.01;
-    let lat_max = lat + shift;
-    let lat_min = lat - shift;
     let lon_max = lon + shift;
     let lon_min = lon - shift;
+    let lat_max = lat + shift;
+    let lat_min = lat - shift;
 
-    let polygon_str = format!("{lon_min} {lat_min}, {lon_min} {lat_max}, {lon_max} {lat_max}, {lon_max} {lat_min}, {lon_min} {lat_min}", );
-    let where_geometry = format!("ST_Within(ST_GeomFromWkb(geometry), ST_Envelope(ST_GeomFromText('POLYGON(({polygon_str}))')))");
+    // let polygon_str = format!("{lon_min} {lat_min}, {lon_min} {lat_max}, {lon_max} {lat_max}, {lon_max} {lat_min}, {lon_min} {lat_min}", );
+    // let where_str = format!("ST_Within(ST_GeomFromWkb(geometry), ST_Envelope(ST_GeomFromText('POLYGON(({polygon_str}))')))");
+    let where_str = format!("bbox.minX > {lon_min} AND bbox.minY > {lat_min} AND bbox.maxX < {lon_max} AND bbox.maxY < {lat_max}");
 
     let path = "./data.duckdb";
     let conn = Connection::open(&path).unwrap();
@@ -22,7 +23,7 @@ pub fn cache_location(lon: f64, lat: f64, name: &str) {
         "read_parquet('../overture/theme=transportation/type=segment/*')".to_string();
     let mut stmt = conn
         .prepare(&format!(
-            "COPY (SELECT * FROM {from_segment} WHERE {where_geometry})
+            "COPY (SELECT * FROM {from_segment} WHERE {where_str})
             TO 'parquet/{lonlatname}_transportation.parquet' (FORMAT 'parquet')"
         ))
         .unwrap();
@@ -31,7 +32,7 @@ pub fn cache_location(lon: f64, lat: f64, name: &str) {
     let from_building = "read_parquet('../overture/theme=buildings/type=building/*')".to_string();
     let mut stmt = conn
         .prepare(&format!(
-            "COPY (SELECT * FROM {from_building} WHERE {where_geometry})
+            "COPY (SELECT * FROM {from_building} WHERE {where_str})
             TO 'parquet/{lonlatname}_building.parquet' (FORMAT 'parquet')"
         ))
         .unwrap();
