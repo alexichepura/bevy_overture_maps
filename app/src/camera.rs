c28e762819acfed33f5f250c8e083ba653fd5e9c
Alexi Chepura <a.chepura@gmail.com>
2026-05-10 10:18:12 +0300

    atmosphere

--- app/src/camera.rs
+++ app/src/camera.rs
@@ -1,6 +1,15 @@
-use bevy::{camera_controller::free_camera::FreeCamera, prelude::*};
+use bevy::{
+    anti_alias::fxaa::Fxaa,
+    camera::Exposure,
+    camera_controller::free_camera::FreeCamera,
+    light::{AtmosphereEnvironmentMapLight, VolumetricFog},
+    pbr::{Atmosphere, AtmosphereSettings, ScatteringMedium, ScreenSpaceReflections},
+    post_process::bloom::Bloom,
+    prelude::*,
+};
 
 // https://github.com/bevyengine/bevy/blob/latest/examples/camera/free_camera_controller.rs
+// https://github.com/bevyengine/bevy/blob/latest/examples/3d/atmosphere.rs
 
 pub struct CameraPlugin;
 impl Plugin for CameraPlugin {
@@ -9,16 +18,32 @@ impl Plugin for CameraPlugin {
     }
 }
 
-fn spawn_camera(mut commands: Commands) {
+fn spawn_camera(mut commands: Commands, mut scattering_mediums: ResMut<Assets<ScatteringMedium>>) {
     commands.spawn((
         Camera3d::default(),
         Transform::from_xyz(0.0, 1.0, 0.0).looking_to(Vec3::X, Vec3::Y),
+        Atmosphere::earthlike(scattering_mediums.add(ScatteringMedium::default())),
+        AtmosphereSettings::default(),
+        // The directional light illuminance used in this scene
+        // (the one recommended for use with this feature) is
+        // quite bright, so raising the exposure compensation helps
+        // bring the scene to a nicer brightness range.
+        Exposure { ev100: 13.0 },
+        Bloom::NATURAL,
+        AtmosphereEnvironmentMapLight::default(),
         FreeCamera {
             sensitivity: 0.2,
             friction: 25.0,
             walk_speed: 10.0,
-            run_speed: 50.0,
+            run_speed: 100.0,
             ..default()
         },
+        VolumetricFog {
+            ambient_intensity: 0.0,
+            ..default()
+        },
+        Msaa::Off,
+        Fxaa::default(),
+        ScreenSpaceReflections::default(),
     ));
 }
--- app/src/light.rs
+++ app/src/light.rs
@@ -1,4 +1,9 @@
-use bevy::prelude::*;
+use bevy::{
+    light::{light_consts::lux, CascadeShadowConfigBuilder, FogVolume, VolumetricLight},
+    prelude::*,
+};
+
+// https://github.com/bevyengine/bevy/blob/latest/examples/3d/atmosphere.rs
 
 use crate::config::SceneConfig;
 
@@ -8,29 +13,60 @@ pub fn light_start_system(
     mut materials: ResMut<Assets<StandardMaterial>>,
     scene_config: Res<SceneConfig>,
 ) {
+    // Configure a properly scaled cascade shadow map for this scene (defaults are too large, mesh units are in km)
+    let cascade_shadow_config = CascadeShadowConfigBuilder {
+        first_cascade_far_bound: 0.3,
+        maximum_distance: 1000.0,
+        ..default()
+    }
+    .build();
+
+    // Sun
     cmd.spawn((
         DirectionalLight {
-            illuminance: 40_000.,
             shadows_enabled: true,
+            // lux::RAW_SUNLIGHT is recommended for use with this feature, since
+            // other values approximate sunlight *post-scattering* in various
+            // conditions. RAW_SUNLIGHT in comparison is the illuminance of the
+            // sun unfiltered by the atmosphere, so it is the proper input for
+            // sunlight to be filtered by the atmosphere.
+            illuminance: lux::RAW_SUNLIGHT,
             ..default()
         },
-        Transform {
-            translation: Vec3::new(0., 0., 0.),
-            rotation: Quat::from_rotation_x(-std::f32::consts::FRAC_PI_8),
-            ..default()
-        },
+        Transform::from_xyz(1.0, 0.4, 0.0).looking_at(Vec3::ZERO, Vec3::Y),
+        VolumetricLight,
+        cascade_shadow_config,
     ));
 
+    // spawn the fog volume
     cmd.spawn((
-        Mesh3d(meshes.add(Cuboid::default().mesh())),
-        MeshMaterial3d(materials.add(StandardMaterial {
-            base_color: Color::srgb(0.53, 0.53, 0.53),
-            unlit: true,
-            cull_mode: None,
-            ..default()
-        })),
-        Transform::from_scale(Vec3::splat(scene_config.size)),
+        FogVolume::default(),
+        Transform::from_scale(Vec3::new(10.0, 1.0, 10.0)).with_translation(Vec3::Y * 0.5),
     ));
+
+    // cmd.spawn((
+    //     DirectionalLight {
+    //         illuminance: 40_000.,
+    //         shadows_enabled: true,
+    //         ..default()
+    //     },
+    //     Transform {
+    //         translation: Vec3::new(0., 0., 0.),
+    //         rotation: Quat::from_rotation_x(-std::f32::consts::FRAC_PI_8),
+    //         ..default()
+    //     },
+    // ));
+
+    // cmd.spawn((
+    //     Mesh3d(meshes.add(Cuboid::default().mesh())),
+    //     MeshMaterial3d(materials.add(StandardMaterial {
+    //         base_color: Color::srgb(0.53, 0.53, 0.53),
+    //         unlit: true,
+    //         cull_mode: None,
+    //         ..default()
+    //     })),
+    //     Transform::from_scale(Vec3::splat(scene_config.size)),
+    // ));
 }
 
 const K: f32 = 2.;
